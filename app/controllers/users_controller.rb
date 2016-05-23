6edc9a64264def95e2c6242546f9e8907ee00aff
jeffreybaird <jeff@jeffreyleebaird.com>
2016-05-23 12:32:34 -0400

    adding a user workflow

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    puts user_params
+    @user = User.find_by(email: user_params[:email]).try(:authenticate, user_params[:password])
+    unless @user
+      @user = User.create(user_params)
+      session[:current_user_id] = @user.id
+      redirect_to "users/registration/#{session[:current_user_id]}"
+    end
+    session[:current_user_id] = @user.id
+    redirect_to "/users/#{session[:current_user_id]}"
+  end
+
+  def registration
+    @user = User.find(session[:current_user_id])
+  end
+
+  def update
+    @user = User.find(session[:current_user_id])
+    @user.update(user_params)
+    redirect_to "/users/#{session[:current_user_id]}"
+  end
+
+  def show
+    @user_to_view = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email,:password,:website,:fullname)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523143642) do
+ActiveRecord::Schema.define(version: 20160523152744) do
 
   create_table "shipwrecks", force: :cascade do |t|
     t.integer  "user_id"
@@ -30,8 +30,9 @@ ActiveRecord::Schema.define(version: 20160523143642) do
     t.string   "fullname"
     t.string   "email"
     t.string   "website"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
